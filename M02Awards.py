0d64fe295cd274ee62d203acb1e8106a51485b88
JTECHFORSCHOOL <157440039+JTECHFORSCHOOL@users.noreply.github.com>
2024-01-24 20:55:00 -0800

    Add files via upload

--- M02Awards.py
+++ M02Awards.py
@@ -1,14 +1,21 @@
 #Joshua Breeden
 #AwardsAuto
 #Check to see if you have made an award
+
+#on/off switch value set
 check = True
+#body
 while check == True:
+    #set up
     Award = "do not qualifiy"
     NameLast = str(input("Your last name"))
+    #on/off switch code placed here to make it less complicated to exit
     if NameLast == "ZZZ":
         break
+    #more set up
     NameFirst = str(input("Your first name"))
     GPA = float(input("Your GPA"))
+    #The brains
     if 3.25<=GPA:
         Award = " are in Honor Roll"
     if GPA>=3.5:
